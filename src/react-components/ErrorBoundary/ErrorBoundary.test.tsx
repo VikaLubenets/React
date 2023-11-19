19438cd7c964390d046c25a5300373fbe4151359
Vika Lubenets <lubenetcvika@gmail.com>
2023-11-19 21:06:55 +0400

    feat: add new test for error boundary

---
+++ src/react-components/ErrorBoundary/ErrorBoundary.test.tsx
@@ -0,0 +1,38 @@
+import React from 'react';
+import { render, waitFor, screen } from '@testing-library/react';
+import ErrorBoundary from './ErrorBoundary';
+import userEvent from '@testing-library/user-event';
+import ErrorButton from '../ErrorButton/ErrorButton';
+import '@testing-library/jest-dom';
+
+const FallbackComponent = () => <div>Error!</div>;
+
+test('ErrorBoundary renders children when there is no error', () => {
+  const { getByText } = render(
+    <ErrorBoundary fallback={<FallbackComponent />}>
+      <div>Content without errors</div>
+    </ErrorBoundary>
+  );
+
+  const content = getByText('Content without errors');
+  expect(content).toBeInTheDocument();
+});
+
+test('ErrorBoundary renders fallback component when there is an error', async () => {
+  jest.spyOn(console, 'error').mockImplementation(() => {});
+
+  const { getByText } = render(
+    <ErrorBoundary fallback={<FallbackComponent />}>
+      <div>
+        <ErrorButton />
+      </div>
+    </ErrorBoundary>
+  );
+
+  const throwBtn = getByText('Throw Error');
+  await userEvent.click(throwBtn);
+
+  await waitFor(() => {
+    expect(screen.getByText('Error!')).toBeInTheDocument();
+  });
+});
